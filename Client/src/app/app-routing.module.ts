328c4d18489ac97e3a353b91d5e30f0d15390bbd
unknown <Aulas Ingenieria@12C302E10210698.JAVERIANA.LOC>
2023-04-10 12:11:27 -0500

    Rest 2

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-
+import { NotfoundComponent } from './components/not-found/notfound.component';
+import { ConductorListComponent } from './components/conductor/conductor-list/conductor-list.component';
 const routes:Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', pathMatch: 'full', redirectTo: '/home' },
-  
+  { path: '**', component: NotfoundComponent},
+  { path: 'conductor/list', component: ConductorListComponent },
+
 ]; 
 
 @NgModule({
